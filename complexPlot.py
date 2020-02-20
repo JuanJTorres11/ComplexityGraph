19573daafe564b2abdd1abb00b86a3143e4e8ba7
Juan José Torres <juanjtorres@pm.me>
2020-02-20 10:50:50 -0500

    Arreglo sobreescritura de array

--- complexPlot.py
+++ complexPlot.py
@@ -12,15 +12,17 @@ def log(x,y):
 
 
 def array_log(array):
+    array2 = array.copy()
     for i in range(len(array)):
-        array[i] = m.log(array[i], 2)
-    return array
+        array2[i] = m.log(array[i], 2)
+    return array2
 
 
 def nlog(array):
+    array2 = array.copy()
     for i in range(len(array)):
-        array[i] = array[i] * m.log(array[i], 2)
-    return array
+        array2[i] = array[i] * m.log(array[i], 2)
+    return array2
 
 
 def plot():
@@ -28,17 +30,26 @@ def plot():
     plt.xlabel("Número de datos ingresados")
     plt.ylabel("Tiempo transcurrido")
     t = np.linspace(xpoints[0], xpoints[len(xpoints) - 1])
+    
+    print ("Los datos en x son:")
+    print (t)
     plt.plot(xpoints, ypoints, 'or', label='Datos Propios')
     plt.plot(t, t, label='Lineal')
     plt.plot(t, t ** 2, label='Cuadratico')
+    print ("Los datos cuadraticos son: \n")
+    print ( t**2)
     plt.plot(t, array_log(t), label='Logaritimico')
-    plt.plot(t, nlog(t), label='Logaritimico')
+    print ("Los datos logaritmicos son: \n")
+    print (array_log(t))
+    plt.plot(t, nlog(t), label='n * Logaritimico')
+    print ("Los datos n*log son: \n")
+    print (nlog(t))
 
     plt.show()
 
 
-log(10, 10)
-log(20,20)
-log(30,30)
-log(60,60)
+log(10, 90)
+log(20,420)
+log(30,850)
+log(60,3700)
 plot()
